12bef79a4b078276ab5fc046f84035db8faf2900
Lucased22 <lucaseduardo.siq@gmail.com>
2023-11-16 14:43:16 -0400

    Initial Commit

---
+++ SolidConcepts-SwiftUI/SolidConcepts_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SolidConcepts_SwiftUIApp.swift
+//  SolidConcepts-SwiftUI
+//
+//  Created by Lucas Santos on 16/11/23.
+//
+
+import SwiftUI
+
+@main
+struct SolidConcepts_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
